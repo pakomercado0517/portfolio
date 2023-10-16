d86199c1f20b04b2fabeba5143097bf07d0f8f44
pakomercado0517 <sissyyayle0517@hotmail.com>
2023-10-16 17:49:50 -0600

    change to currency input on payback page

--- src/pages/PaybackInformation.jsx
+++ src/pages/PaybackInformation.jsx
@@ -1,16 +1,15 @@
 import { useState, useEffect } from "react";
+import PaybackForm from "../components/PaybackForm";
+import PaybackTable from "../components/PaybackTable";
 import useCreatePDF from "../hooks/useCreatePDF";
 import useFormatNumber from "../hooks/useFormatNumber";
-import PaybackTable from "../components/PaybackTable";
-import { Button } from "flowbite-react";
 
 function PaybackInformation() {
-  const createDocument = useCreatePDF();
-  const formatNumber = useFormatNumber();
-  const [data, setData] = useState({});
-  const [formattedNumber, setFormattedNumber] = useState("");
   const [showTable, setShowTable] = useState(false);
   const [getTotal, setGetTotal] = useState([]);
+  const [data, setData] = useState({});
+  const createDocument = useCreatePDF();
+  const formatNumber = useFormatNumber();
 
   useEffect(() => {
     if (data.length !== 0) {
@@ -36,6 +35,13 @@ function PaybackInformation() {
     });
   };
 
+  const handleCurrencyInput = (value) => {
+    setData({
+      ...data,
+      total: value === undefined ? "" : value,
+    });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -49,73 +55,15 @@ function PaybackInformation() {
       <h1 className="text-3xl my-10 text-center underline">
         Obtener monto a devolver
       </h1>
-      <form className="mt-5">
-        <div className="relative z-0 w-full mb-6 group">
-          <input
-            onChange={handleChange}
-            type="text"
-            name="name"
-            id="name"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            required=""
-          />
-          <label
-            htmlFor="name"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Nombre de la empresa
-          </label>
-        </div>
-
-        <div className="grid md:grid-cols-2 md:gap-6">
-          <div className="relative z-0 w-full mb-6 group">
-            <input
-              onChange={handleChange}
-              type="number"
-              name="percentage"
-              id="percentage"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required=""
-              defaultValue={data.percentage}
-            />
-            <label
-              htmlFor="percentage"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Porcentaje
-            </label>
-          </div>
-          <div className="relative z-0 w-full mb-6 group">
-            <input
-              onChange={handleChange}
-              type="text"
-              name="total"
-              id="total"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder=" "
-              required=""
-            />
-            <label
-              htmlFor="total"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Monto
-            </label>
-          </div>
-        </div>
-        <div className="flex justify-center gap-4">
-          <button
-            type="submit"
-            onClick={handleSubmit}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Crear PDF
-          </button>
-          <Button onClick={handleShowTable}>Mostrar Resultados</Button>
-        </div>
-      </form>
+      <article>
+        <PaybackForm
+          handleChange={handleChange}
+          data={data}
+          handleSubmit={handleSubmit}
+          handleShowTable={handleShowTable}
+          handleCurrencyInput={handleCurrencyInput}
+        />
+      </article>
       <article className={`${!showTable ? "hidden" : ""} mt-10`}>
         <PaybackTable data={getTotal} />
       </article>
